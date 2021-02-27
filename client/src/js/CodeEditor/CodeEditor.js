efa990f7e1bc2241a79171e33306a1b5229a06d9
João Vilaça <machadovilaca@gmail.com>
2021-02-27 16:36:44 +0000

    Add close button

--- client/src/js/CodeEditor/CodeEditor.js
+++ client/src/js/CodeEditor/CodeEditor.js
@@ -4,36 +4,43 @@ import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "./CodeEditor.css";
-import {fileOpen, fileSave} from "browser-fs-access"
-import CanvasDraw from "react-canvas-draw"
+import { fileOpen, fileSave } from "browser-fs-access";
+import CanvasDraw from "react-canvas-draw";
 
 class CodeEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { code: "", name: "" , socket: props.socket, filename: "", blob: null, canvas: ""};
 
-    this.inputchange = this.props.inputchange.bind(this)
-    this.canvasinputchange = this.props.canvasinputchange.bind(this)
+    this.state = {
+      code: "",
+      name: "",
+      socket: props.socket,
+      filename: "",
+      blob: null,
+    };
 
-    this.openFile = this.openFile.bind(this)
-    this.updateCanvas = this.updateCanvas.bind(this)
+    this.inputchange = this.props.inputchange.bind(this);
+    this.canvasinputchange = this.props.canvasinputchange.bind(this);
+
+    this.openFile = this.openFile.bind(this);
+    this.updateCanvas = this.updateCanvas.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.state.socket
       .on("file", (data) => {
         this.setState({ code: data.data });
       })
       .on("canvas", (data) => {
         this.setState({
-          canvas: data.data
-        })
-      })
+          canvas: data.data,
+        });
+      });
   }
 
   async openFile() {
     const blob = await fileOpen({
-      mimeTypes: ['text/plain'],
+      mimeTypes: ["text/plain"],
     });
 
     const reader = new FileReader();
@@ -41,63 +48,79 @@ class CodeEditor extends React.Component {
       this.setState({
         code: e.target.result,
       });
-      this.inputchange(e.target.result)
-     };
+      this.inputchange(e.target.result);
+    };
     this.setState({
       filename: blob.name,
-      blob: blob
+      blob: blob,
     });
 
     reader.readAsText(blob);
-  };
+  }
 
   saveFile(event) {
     var blob = new Blob([this.state.code], {
       type: "text/plain;charset=utf-8",
     });
-    const handle = this.state.blob.handle
+    const handle = this.state.blob.handle;
     fileSave(blob, {}, handle);
   }
 
   updateEditor(code) {
     this.setState({
-      code: code
-    })
-    this.inputchange(code)
+      code: code,
+    });
+    this.inputchange(code);
+  }
+
+  closeFile() {
+    this.inputchange("");
+    this.setState({ blob: null, code: "" });
   }
 
   updateCanvas(canvas) {
     this.setState({
-      canvas: canvas.getSaveData()
-    })
-    this.canvasinputchange(canvas.getSaveData())
+      canvas: canvas.getSaveData(),
+    });
+    this.canvasinputchange(canvas.getSaveData());
   }
 
   loadCanvas(event) {
-    console.log("reloading canvas")
-    event.loadSaveData(this.state.canvas, true)
+    console.log("reloading canvas");
+    event.loadSaveData(this.state.canvas, true);
   }
 
   render() {
     return (
       <div>
         <div>
-          <button type="file" onClick={this.openFile}>Open</button>
-          {this.state.blob ? <button onClick={(e) => this.saveFile(e)}>Save</button> : "" }
+          <button type="file" onClick={this.openFile}>
+            Edit file
+          </button>
+          {this.state.blob ? (
+            <div>
+              <button onClick={(e) => this.saveFile(e)}>Save</button>
+              <button onClick={(e) => this.closeFile()}>Close</button>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         <div>
-          <CanvasDraw gridColor="rgba(255,255,255,0)"
-            onLoad={(event) => (this.loadCanvas(event))}
+          <CanvasDraw
+            gridColor="rgba(255,255,255,0)"
+            onLoad={(event) => this.loadCanvas(event)}
             onChange={this.updateCanvas}
-            hideGrid={true} 
-            canvasWidth={400} 
-            canvasHeight={400} 
-            brushRadius={2} />
+            hideGrid={true}
+            canvasWidth={400}
+            canvasHeight={400}
+            brushRadius={2}
+          />
         </div>
-        <div>
+        <div className="draw-canvas">
           <Editor
             value={this.state.code}
-            onValueChange={(code) => this.updateEditor( code )}
+            onValueChange={(code) => this.updateEditor(code)}
             highlight={(code) => highlight(code, languages.js)}
             padding={10}
             style={{
