85eeb71c6d9155337f3bcff5df9fae6b84f10217
Filipe Monteiro <pimonteiro@protonmail.com>
2021-02-28 04:52:25 +0000

    Update ringtone

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     this.rejectCallHandler = this.rejectCall.bind(this);
     this.writeFile = this.writeFile.bind(this);
     this.writeCanvas = this.writeCanvas.bind(this);
-    this.audio = new Audio("client/src/sounds/skype.mp3");
+    this.audio = new Audio("client/src/sounds/talk_code_to_me.mp3");
     this.audio.load()
   }
 
